c1e63fc578707647c3ba46426a428e15efa8b9b9
jSreih <joseph.e.sreih@gmail.com>
2023-05-20 17:05:52 -0400

    Update Title.js

--- cataglyphis/src/Title.js
+++ cataglyphis/src/Title.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
 const Title = () => {
+  const titleStyle = {
+    textAlign: 'center',
+    color: 'black',
+    fontFamily: 'Arial, sans-serif',
+    fontSize: '48px',
+    textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',
+  };
+
   return (
-    <h1 style={{ textAlign: 'center' }}>Cataglyphis</h1>
+    <h1 style={titleStyle}>Cataglyphis</h1>
   );
 };
 
